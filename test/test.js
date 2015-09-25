023e534ba9a40ed6b10eaa2881d11379f5a9b634
David Wee <rook2pawn@gmail.com>
2015-09-25 02:55:26 -0700

    update tape tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+var test = require('tape');
 var Treelib = require('../index');
 var tree = Treelib();
 
@@ -16,62 +17,57 @@ var pathD_string = '2/cat';
 var pathE_array = [2,'cat','dog'];
 var pathE_string = '2/cat/dog';
 
-exports.testBasics = function(test) {
-	test.expect(6);
-	tree.path(pathA_string);
-	test.deepEqual(tree.checkPath('a/b/cauliflower'),
-		{depth:2,validPath:['a','b']}, "Failed deep equal (test 1)");
-	test.deepEqual(tree.checkPath('a/b/c'),
-		{depth:3,validPath:['a','b','c']}, "Failed deep equal (test 2)");
-	test.deepEqual(tree.tree(), {a:{b:{c:undefined}}});
-	tree.path(pathA_array);
-	test.deepEqual(tree.checkPath('a/b/cauliflower'),
-		{depth:2,validPath:['a','b']}, "Failed deep equal (test 1)");
-	test.deepEqual(tree.checkPath('a/b/c'),
-		{depth:3,validPath:['a','b','c']}, "Failed deep equal (test 2)");
-	test.deepEqual(tree.tree(), {a:{b:{c:undefined}}});
-	test.done();
-};
-exports.testOverwrite = function(test) {
-	test.expect(1);
+test('basics', function (t) {
+    t.plan(6);
+    tree.path(pathA_string);
+    t.deepEqual(tree.checkPath('a/b/cauliflower'),
+      {depth:2,validPath:['a','b']})
+    t.deepEqual(tree.checkPath('a/b/c'),
+      {depth:3,validPath:['a','b','c']})
+    t.deepEqual(tree.tree(), {a:{b:{c:undefined}}});
+    tree.path(pathA_array);
+    t.deepEqual(tree.checkPath('a/b/cauliflower'),
+      {depth:2,validPath:['a','b']})
+    t.deepEqual(tree.checkPath('a/b/c'),
+      {depth:3,validPath:['a','b','c']})
+    t.deepEqual(tree.tree(), {a:{b:{c:undefined}}});
+});
+test('overwrite',function(t) {
+	t.plan(1);
 	tree.clear();
 	tree.path(pathB_string);
 	tree.path(pathB_array);
-	test.deepEqual(tree.tree(),{2:{3:{5:{7:undefined}}}});
-	test.done();
-};
-exports.testBranching = function(test) {
-	test.expect(1);
+	t.deepEqual(tree.tree(),{2:{3:{5:{7:undefined}}}});
+})
+
+test('branching',function(t) {
+	t.plan(1);
 	tree.clear();
 	tree.path(pathB_string);
 	tree.path(pathC_string);
-	test.deepEqual(tree.tree(),{2:{3:{foo:{bar:undefined}, 5:{7:undefined}}}});
+	t.deepEqual(tree.tree(),{2:{3:{foo:{bar:undefined}, 5:{7:undefined}}}});
 	
-	test.done();
-};
+})
 
-exports.testBranching2 = function(test) {
-	test.expect(1);
+test('branching2',function(t) {
+	t.plan(1);
 	tree.clear();
 	tree.path(pathD_string);
 	tree.path(pathE_array);
-	test.deepEqual(tree.tree(),{2:{cat:{dog:undefined}}});
-	
-	test.done();
-};
+	t.deepEqual(tree.tree(),{2:{cat:{dog:undefined}}});
+})
 
-exports.testSetAndClearValues = function(test) {
-	test.expect(5);
+test('set and clear values',function(t) {
+  t.plan(5)
 	tree.clear();
 	tree.path(pathD_string);
 	tree.setValue('meows');
-	test.equal('meows',tree.getValue('2/cat'));
-	test.equal('meows',tree.getValue(['2','cat']));
+	t.equal('meows',tree.getValue('2/cat'));
+	t.equal('meows',tree.getValue(['2','cat']));
 	tree.clearValue('2/cat');
-	test.equal(undefined, tree.getValue(['2','cat']));
+	t.equal(undefined, tree.getValue(['2','cat']));
 	tree.path('2/cat').setValue('karma');
-	test.notEqual(undefined,tree.getValue('2/cat'));
-	test.equal('karma',tree.getValue('2/cat'));
+	t.notEqual(undefined,tree.getValue('2/cat'));
+	t.equal('karma',tree.getValue('2/cat'));
 	// and mix in a path test
-	test.done();
-};
+})
